78563c1a5cac54376806fc547b171de627a9d625
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2023-11-15 20:17:26 +0800

    commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import schedule
+import time
+from datetime import datetime
+def job():
+    print("I'm working...")
+    print(datetime.now())
+    f = open("demofile2.txt", "a")
+    f.write(f"bla bla")
+    f.close()
+
+schedule.every(10).minutes.do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
